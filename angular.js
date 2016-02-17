fd397fe43e0105c22578266cbf68cac931186df9
Yvonne Madera <yvonnemadera@Macintosh.local>
2016-02-17 15:45:16 -0500

    adding modified js file

---
+++ angular.js
@@ -0,0 +1,13 @@
+angular.module('MyApp', [])
+
+.controller('MainController' , function() {
+	var self = this;
+
+	self.classObject = {
+		red: false,
+		green: false,
+		yellow: false,
+		black: false
+	}
+
+});
